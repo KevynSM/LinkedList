7e05ad488d85ee41988d3cc0b5a6d45e7152f405
Kevyn Marins <kevynmarins@gmail.com>
2020-10-16 19:22:58 +0100

    Add SingleListNode

---
+++ src/com/company/SingleListNode.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class SingleListNode {
+    private Object element;
+    private SingleListNode nextNode;
+
+    SingleListNode(Object element, SingleListNode nextNode) {
+        this.element = element;
+        setNext(nextNode);
+    }
+
+    public Object getElement() {
+        return this.element;
+    }
+
+    public Object getNext() {
+        return this.nextNode;
+    }
+
+    public void setNext(SingleListNode node) {
+        this.nextNode = node;
+    }
+
+}
